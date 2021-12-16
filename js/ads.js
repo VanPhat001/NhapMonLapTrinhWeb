8fab02b2ed6db4c46c8da523d7ec1c167ed94811
NguyenVanPhat <vanphatct2002@gmail.com>
2021-12-16 23:53:07 +0700

    add product-full-header

--- js/ads.js
+++ js/ads.js
@@ -8,27 +8,35 @@ var adsList = [
 ];
 let adsCurrent = 0;
 let adsWrap = document.querySelector('.ads__wrap');
-adsWrap.querySelector('.ads__img').style.backgroundImage = `url(${adsList[0]})`;
+adsWrap.querySelector('.ads__img').style.backgroundImage = `url(${adsList[0].photo})`;
 adsWrap.querySelector('.ads__img').style.animation = 'fadeIn ease 1s';
 
-setInterval(() => {
-    adsWrap.removeChild(adsWrap.querySelector('.ads__img'));
+let intervalId = setInterval(showAds, 6500);
 
+function showAds(increment = 1, animation1 = 'fadeOutToLeft', animation2 = 'fadeInToLeft') {
+    // xóa quảng cáo hiện tại 
+    $('.ads__img').remove();
+
+    let div = createAds(adsCurrent);
+    div.style.animation = animation1 + ' ease 0.75s';
+    adsWrap.appendChild(div);
+
+    adsCurrent += increment;
+    if (adsCurrent >= adsList.length) adsCurrent = 0;
+    if (adsCurrent < 0) adsCurrent = adsList.length - 1;
+
+    // hiển thị quảng cáo mới     
+    div = createAds(adsCurrent);
+    div.style.animation = animation2 + ' ease 0.75s';
+    adsWrap.appendChild(div);
+}
+
+function createAds(adsCurrent) {
     let div = document.createElement('div');
     let path = adsList[adsCurrent].photo;
     div.className = 'ads__img';
     div.style.backgroundImage = `url(${path})`;
-    div.style.animation = 'fadeOut ease 1s';
-    adsWrap.appendChild(div);
-    
-    adsCurrent++;
-    if (adsCurrent >= adsList.length) adsCurrent = 0;
+    // div.style.animation = 'fadeOut ease 1s';
 
-    div = document.createElement('div');
-    path = adsList[adsCurrent].photo;
-    div.className = 'ads__img';
-    div.style.backgroundImage = `url(${path})`;
-    div.style.animation = 'fadeIn ease 1s';
-    adsWrap.appendChild(div);
-
-}, 5000);
+    return div;
+}
