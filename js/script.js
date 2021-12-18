16c1ebf17749d8b8af475fc0eab527dfbbd053e0
NguyenVanPhat <vanphatct2002@gmail.com>
2021-12-19 03:18:54 +0700

    add feature validateForm()

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 // 'use strict'
-
-
 var productList = {
-    sp001 : {
+    sp001: {
         id: 'sp001',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -15,7 +13,7 @@ var productList = {
         rate: 4,
         like: true
     },
-    sp002 : {
+    sp002: {
         id: 'sp002',
         name: '[Mới 100%] Laptop Dell Inspiron 14 5415 (Ryzen R5-5500U, RAM 8G, SSD 256G, AMD Radeon™ Graphics, màn 14.0 Full HD, IPS)',
         imageList: [
@@ -28,7 +26,7 @@ var productList = {
         rate: 5,
         like: false
     },
-    sp003 : {
+    sp003: {
         id: 'sp003',
         name: 'Đồng Hồ Thông Minh HUAWEI WATCH GT 3 46mm |Theo Dõi Spo2 Cả Ngày | Huấn Luyện Viên Chạy Bộ AI',
         imageList: [
@@ -41,7 +39,7 @@ var productList = {
         rate: 5,
         like: false
     },
-    sp004 : {
+    sp004: {
         id: 'sp004',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -54,7 +52,7 @@ var productList = {
         rate: 4,
         like: false
     },
-    sp005 : {
+    sp005: {
         id: 'sp005',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -67,7 +65,7 @@ var productList = {
         rate: 2,
         like: false
     },
-    sp006 : {
+    sp006: {
         id: 'sp006',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -80,7 +78,7 @@ var productList = {
         rate: 1,
         like: false
     },
-    sp007 : {
+    sp007: {
         id: 'sp007',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -93,7 +91,7 @@ var productList = {
         rate: 4,
         like: false
     },
-    sp008 : {
+    sp008: {
         id: 'sp008',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -106,7 +104,7 @@ var productList = {
         rate: 4,
         like: false
     },
-    sp009 : {
+    sp009: {
         id: 'sp009',
         name: 'Máy Chiếu Phim Mini Beecube X1 Độ Phân Giải HD',
         imageList: [
@@ -120,6 +118,13 @@ var productList = {
         like: false
     }
 };
+
+var account = [
+    {taikhoan: 'user1@gmail.com', matkhau: '12345678'},
+    {taikhoan: 'user2@gmail.com', matkhau: '12345678'},
+    {taikhoan: 'user3@gmail.com', matkhau: '12345678'},
+    {taikhoan: 'user4@gmail.com', matkhau: '12345678'}
+];
 
 // hiển thị danh sách sản phẩm trên trang chủ
 // console.log(productList);
@@ -141,7 +146,47 @@ for (const key in productList) {
     }
 }
 
+function findAccount(frm) { 
+    let taikhoan = frm.username.value;
+    let matkhau = frm.pass.value;
+    let save = frm.save.checked;
 
+    for (let i = 0; i < account.length; i++)
+    {
+        let element = account[i];
+        if (element.taikhoan == taikhoan && element.matkhau == matkhau)
+        {
+            alert('Đăng nhập thành công');
+
+            if (save) {
+                localStorage.setItem('indexAccount', i);
+            }
+
+            frm.pass.value = '';
+            return false;
+        }
+    }
+
+    alert('tài khoản hoặc mật khẩu sai');
+    frm.pass.value = '';
+    frm.pass.focus();
+    return false; 
+}
+
+function validate(frm) {
+
+    if (typeof frm.pass2.value !== "undefined" && frm.pass2.value != frm.pass.value)
+    {
+        alert('Mật khẩu không khớp!');
+        frm.pass2.focus();
+        return false;
+    }
+
+    alert('đăng ký thành công');
+    frm.pass.value = '';
+    frm.pass2.value = '';
+    return false; // không cho gửi dữ liệu :)
+}
 
 
 // function min(a, b) {
@@ -150,17 +195,17 @@ for (const key in productList) {
 
 $(document).ready(function () {
     // lưu thông tin để mở trang sản phẩm
-    $('.product-box a.product').click(function (e) { 
+    $('.product-box a.product').click(function (e) {
         // console.log(productList[this.id]);        
         localStorage.setItem('productId', this.id);
     });
 
     // chuyển quảng cáo
-    $('.btn-left-ads').click(function (e) { 
-        showAds();        
+    $('.btn-left-ads').click(function (e) {
+        showAds();
     });
 
-    $('.btn-right-ads').click(function (e) { 
+    $('.btn-right-ads').click(function (e) {
         showAds(-1, 'fadeOutToRight', 'fadeInToRight');
     });
 
@@ -196,7 +241,7 @@ $(document).ready(function () {
 
     });
 
-    $('.select-language .btn-language').click(function (e) { 
+    $('.select-language .btn-language').click(function (e) {
         $('.select-language .btn--select').removeClass('btn--select');
         $(this).addClass('btn--select');
     });
